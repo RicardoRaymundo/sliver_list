58a72e376dbaf9a7a0cec1cf060c8579b4c608c4
Raymundo <ricardoraymundao@gmail.com>
2019-06-27 09:19:52 -0300

    Adicionado um SliverGrid abaixo da SliverList e mais comentários

--- lib/main.dart
+++ lib/main.dart
@@ -29,41 +29,66 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: CustomScrollView(slivers: <Widget>[
-          SliverAppBar(
-            //AppBar Sliver
-            title: Text('Alguem me segure!'),
-            leading: Icon(Icons.keyboard_arrow_down),
-            expandedHeight: 250.0,
-            floating: true,
-            pinned: true,
-            flexibleSpace: FlexibleSpaceBar(
-              background:
-                  //Um modo de colocar uma imagem por URL no codigo
-                  Image.network('https://wallpapercave.com/wp/wp1857622.jpg'),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate((context, index) => Card(
-                  child: Container(
-                    padding: EdgeInsets.all(10.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        CircleAvatar(
-                          backgroundImage: NetworkImage(
+      SliverAppBar(
+        title: Text('Alguem me segure!'),
+        leading: Icon(Icons.keyboard_arrow_down),
+        expandedHeight: 190.0,
+        //Propriedades que definem o comportamento da SliverAppBar
+        floating: true,
+        pinned: true,
+        snap: true,
+        flexibleSpace: FlexibleSpaceBar(
+          background:
+              //Um modo de colocar uma imagem por URL no codigo
+              Image.network('https://wallpapercave.com/wp/wp1857622.jpg'),
+        ),
+      ),
+      SliverList(
+        delegate: SliverChildBuilderDelegate(
+          //Construtor dos filhos da SliverList. Cada filho terá as caracteristicas abaixo
+          (BuildContext context, int index) => Card(
+                child: Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(
                             //Outro método para colocar imagem por URL
-                              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5_4IhCaoqoBHp5Da8geUJv67yxLbVhe8wzuVbXG-f7iHkfo9f'),
-                          backgroundColor: Colors.transparent,
-                        ),
-                        SizedBox(
-                          width: 10.0,
-                        ),
-                        Text('CONTEUDO DO CARD!!')
-                      ],
-                    ),
+                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5_4IhCaoqoBHp5Da8geUJv67yxLbVhe8wzuVbXG-f7iHkfo9f'),
+                        backgroundColor: Colors.transparent,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('CONTEUDO DO CARD!!')
+                    ],
                   ),
-                )),
-          )
-        ]));
+                ),
+              ),
+          childCount: 9, //numero de filhos do SliverList
+        ),
+      ),
+      SliverGrid(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          //Propriedades de espaçamento e tamanho dos filhos do grid
+          maxCrossAxisExtent: 100.0,
+          mainAxisSpacing: 10.0,
+          crossAxisSpacing: 10.0,
+          childAspectRatio: 1.0,
+        ),
+        delegate: SliverChildBuilderDelegate(
+          (BuildContext context, int index) {
+            return Container(
+              //Metodo que atribui diferentes tonalidades de uma cor aos itens do grid
+              // conforme eles sao criados
+              color: Colors.lightGreen[100 * (index % 9)],
+            );
+          },
+          childCount: 32,
+          semanticIndexOffset: 4,
+        ),
+      )
+    ]));
   }
 }
